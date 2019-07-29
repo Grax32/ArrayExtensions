3c0f7df6520c08f3632bf5af92cc190c8f5c8762
David Walker <dwalker@vorteon.com>
2019-07-28 23:05:01 -0400

    Initial commit

---
+++ ArrayExtensions/ArrayExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Grax
+{
+    public static class ArrayExtensions
+    {
+        public static void Fill<T>(this T[] destinationArray, params T[] value)
+        {
+            if (destinationArray == null)
+            {
+                throw new ArgumentNullException(nameof(destinationArray));
+            }
+
+            if (value.Length > destinationArray.Length)
+            {
+                throw new ArgumentException("Length of value array must not be more than length of destination");
+            }
+
+            Array.Copy(value, destinationArray, value.Length);
+
+            var copyLength = value.Length;
+            var destinationArrayHalfLength = destinationArray.Length / 2;
+
+            for (; copyLength < destinationArrayHalfLength; copyLength *= 2)
+            {
+                Array.Copy(destinationArray, 0, destinationArray, copyLength, copyLength);
+            }
+
+            Array.Copy(destinationArray, 0, destinationArray, copyLength, destinationArray.Length - copyLength);
+        }
+    }
+}
---
+++ ArrayExtensionsUnitTests/ArrayExtensionsUnitTest.cs
@@ -0,0 +1,61 @@
+using Grax;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace ArrayExtensionsUnitTests
+{
+    [TestClass]
+    public class ArrayExtensionsTests
+    {
+        [TestMethod]
+        public void ArrayFillTest()
+        {
+            var myArray = new int[12000];
+            var filler = new int[] { 1, 2, 3, 4, 5 };
+
+            myArray.Fill(filler);
+
+            Assert.AreEqual(5, myArray.Last());
+        }
+
+        [TestMethod]
+        public void ArrayFillExactDoubleTest()
+        {
+            var myArray = new int[16384];
+
+            myArray.Fill(7);
+
+            Assert.AreEqual(7, myArray.Last());
+        }
+
+        [TestMethod]
+        public void ArrayFillTestUnevens()
+        {
+            for (var i = 0; i < 5; i++)
+            {
+                var myArray = new int[12001 + i];
+                var filler = new int[] { 0, 1, 2, 3, 4, 5 };
+
+                myArray.Fill(filler);
+
+                Assert.AreEqual(i, myArray.Last());
+            }
+        }
+
+        [TestMethod]
+        public void ArrayFillTestGiant()
+        {
+            for (var loopCount = 0; loopCount < 5; loopCount++)
+            {
+                var arrayLength = 2000000000 - 4 + loopCount;
+                var myArray = new byte[arrayLength];
+                var filler = new byte[] { 0, 1, 2, 3, 4 };
+
+                myArray.Fill(filler);
+
+                var lastValue = myArray[arrayLength - 1];
+                Assert.AreEqual(loopCount, lastValue);
+            }
+        }
+    }
+}
